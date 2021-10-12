131bcdc512723ef1801a5adba4b59d5b39b8804d
MattGXT <tianyixingchen@gmail.com>
2021-10-12 21:56:52 +1100

    server auth

--- BackEnd/controller/User.js
+++ BackEnd/controller/User.js
@@ -6,6 +6,7 @@ const db = connection.client.db(dbName);
 const jwt = require('jsonwebtoken')
 var mail = require('./Email')
 var crypto = require("crypto");
+const bcrypt = require('bcrypt')
 
 
 
@@ -20,21 +21,28 @@ module.exports.signUp = async function (req, res) {
     if (query2) {
         return res.send("Email existed")
     }
-    const user = { "email": email, "name": req.body.name, "password": req.body.password, "gender": req.body.gender, "mateId": null }
-    const code = crypto.randomBytes(20).toString('hex')
-    const createdAt = new Date()
-    const expired = new Date(createdAt.setDate(createdAt.getDate() + 5))
-    db.collection("verify").insertOne({ info: user, verify: code, expired: expired.getTime() }, function (err, id) {
-        if (err) return res.sendStatus(406)
-        mail.sendMail(email,code)
-        return res.sendStatus(200)
-    })
+    try {
+        const salt = await bcrypt.genSalt()
+        const hashedPassword = await bcrypt.hash(req.body.password, salt)
+        const user = { "email": email, "name": req.body.name, "password": hashedPassword, "gender": req.body.gender, "mateId": null }
+        const code = crypto.randomBytes(20).toString('hex')
+        const createdAt = new Date()
+        const expired = new Date(createdAt.setDate(createdAt.getDate() + 5))
+        db.collection("verify").insertOne({ info: user, verify: code, expired: expired.getTime() }, function (err, id) {
+            if (err) return res.sendStatus(406)
+            mail.sendMail(email, code)
+            return res.sendStatus(200)
+        })
+    } catch {
+        res.status(500).send()
+    }
+
 }
 
 module.exports.verify = async function (req, res) {
     const time = new Date().getTime()
     const verify = req.query.code
-    if (verify == undefined){
+    if (verify == undefined) {
         return res.sendStatus(406)
     }
     const query = await db.collection("verify").findOne({ "verify": verify })
@@ -45,11 +53,11 @@ module.exports.verify = async function (req, res) {
         const code = crypto.randomBytes(20).toString('hex')
         const createdAt = new Date()
         const expired = new Date(createdAt.setDate(createdAt.getDate() + 5))
-        const result = await db.collection("verify").updateOne({ "verify": verify }, { $set: { "verify": id, "expired": expired.getTime() }})
-        if(result){
-            mail.sendMail(query.info.email,code)
+        const result = await db.collection("verify").updateOne({ "verify": verify }, { $set: { "verify": id, "expired": expired.getTime() } })
+        if (result) {
+            mail.sendMail(query.info.email, code)
         }
-    }else{
+    } else {
         const user = query.info
         const session = connection.client.startSession();
         const transactionOptions = {
@@ -60,7 +68,7 @@ module.exports.verify = async function (req, res) {
         try {
             await session.withTransaction(async () => {
                 await db.collection("users").insertOne(user)
-                await db.collection("verify").deleteOne({"verify": verify})
+                await db.collection("verify").deleteOne({ "verify": verify })
             }, transactionOptions)
         } catch {
             res.send("Create failed")
@@ -74,13 +82,25 @@ module.exports.verify = async function (req, res) {
 module.exports.login = async function (req, res) {
     const email = req.body.email
     const password = req.body.password
-    const user = { email, password }
-    const query = await db.collection("users").findOne(user)
+    const query = await db.collection("users").findOne({ email })
     if (query) {
-        const accessToken = jwt.sign({ email: email, _id: query._id }, process.env.ACCESS_TOKEN_SECRET)
-        return res.json({ "accessToken": accessToken, "mateId": query.mateId, "name": query.name, "email": query.email })
+        try {
+            if (await bcrypt.compare(password, query.password)) {
+                const accessToken = jwt.sign({ email: email, _id: query._id }, process.env.ACCESS_TOKEN_SECRET)
+                return res.send({ "accessToken": accessToken, "mateId": query.mateId, "name": query.name, "email": query.email })
+            } else {
+                res.sendStatus(401)
+            }
+        } catch {
+            res.sendStatus(500)
+        }
     } else {
-        return res.sendStatus(401)
+        const query2 = await db.collection("verify").findOne({ email })
+        if(query2){
+            return res.send("please check your email to confirm the register")
+        }else{
+            return res.sendStatus(400)
+        }
     }
 }
 
